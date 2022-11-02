5b0b0bb9779d77c8e27dc41325466e137affb8c1
shahrierNafis <shahriernafisog@gmail.com>
2022-11-02 16:50:31 +0600

    Make the calculator work!

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function add(a, b) {
 function subtract(a, b) {
     return a - b;
 }
-function multiply() {
-    let product = 1;
-    arguments.forEach(number => {
-        product *= number;
-    });
-    return product;
+function multiply(a, b) {
+    return a * b;
 }
 function divide(a, b) {
+    if (b == 0) {
+        alert("You can't divide by 0!");
+        return 0;
+    };
     return a / b;
 }
 function operate(operator, a, b) {
@@ -19,12 +19,46 @@ function operate(operator, a, b) {
     switch (operator) {
         case "+":
             func = add;
+            break;
         case "-":
             func = subtract;
+            break;
         case "*":
             func = multiply;
+            break;
         case "/":
             func = divide;
+            break;
+        default:
+            return "error";
     }
-    return func(a, b);
+    return Math.round((func(a, b) * 100)) / 100;
 }
+const numbers = document.querySelectorAll(".numbers");
+const display = document.querySelector("#display");
+let operation, number1, number2;
+
+numbers.forEach((number) => number.addEventListener("click", (e) => {
+    if (display.innerText == "0") display.innerText = "";
+    display.innerText += e.target.innerText;
+}));
+const operations = document.querySelectorAll(".operations");
+operations.forEach((operation) => operation.addEventListener("click", (e) => {
+    if ((e.target.innerText == "=") || !(e.target.innerText)) {
+        number2 = Number(display.innerText.split(/[+\-*/]/g)[1]);
+        display.innerText = operate(operation, number1, number2);
+        operation = 0;
+
+    } else {
+        number1 = Number(display.innerText);
+        display.innerText += e.target.innerText;
+        operation = e.target.innerText;
+    }
+}));
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    display.innerText = "0";
+    number1 = 0;
+    number2 = 0;
+    operation = "";
+})
